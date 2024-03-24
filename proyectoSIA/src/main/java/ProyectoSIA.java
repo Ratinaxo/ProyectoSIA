77f34a8cd36e4348d4431fcfe880860d6f409873
ricardo <ricardo.toro.m@mail.pucv.cl>
2024-03-24 16:12:04 -0300

    creado repo

--- proyectoSIA/src/main/java/ProyectoSIA.java
+++ proyectoSIA/src/main/java/ProyectoSIA.java
@@ -1,11 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-/**
- *
- * @author ricar
- */
 public class ProyectoSIA {
 
     public static void main(String[] args) {
