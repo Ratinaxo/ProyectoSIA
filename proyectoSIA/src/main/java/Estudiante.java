42a6a97953d0ce8c8459c1eed47f012f5e5304fb
ricardo <ricardo.toro.m@mail.pucv.cl>
2024-03-27 23:06:25 -0300

    .

--- proyectoSIA/src/main/java/Estudiante.java
+++ proyectoSIA/src/main/java/Estudiante.java
@@ -1,12 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ricar
- */
 public class Estudiante {
+    private int rut;
+    private String nombre;
+
+    public Estudiante(int rut, String nombre) {
+        this.rut = rut;
+        this.nombre = nombre;
+    }
+    
+    
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
