0e07a0f9824fb71d3c0f865cc79c145ffe07b299
migue <migue@LAPTOP-SJP5JB8Q>
2024-03-29 00:03:00 -0300

    Creacion de las funciones agregar para los cursos y para los recursos de los mismos, ademas, de creacion de la funcion para mostrar los datos de los cursos.

--- proyectoSIA/src/main/java/Colegio.java
+++ proyectoSIA/src/main/java/Colegio.java
@@ -19,7 +19,7 @@ class Colegio
     {
         int opcion = 0;
 
-        while (opcion != 7)
+        while (opcion != 4)
         {
             mostrarMenu();
             opcion = Integer.parseInt(lector.readLine());
@@ -31,27 +31,41 @@ class Colegio
                     agregarCurso();
                     break;
                 }
-                
+                case 2:
+                {
+                    agregarRecursoACurso();
+                    break;
+                }
+                case 3:
+                {
+                    mostrarDatosCursos();
+                    break;
+                }
+                case 4:
+                {
+                    System.out.println("Adiós, que tenga un buen día");
+                    break;
+                }
+                default:
+                    System.out.println("Opción no válida, por favor seleccione otra opción.");
             }
         }
     }
-    
-
-    //FUNCION MENU
+   
+    /********************************************************************************** */
+    //FUNCION DE MENU
     private void mostrarMenu()
     {
         System.out.println("\nSeleccione qué acción quiere realizar:\n");
         System.out.println("1- Agregar curso");
-        System.out.println("2- Buscar curso");
-        System.out.println("3- Eliminar curso");
-        System.out.println("4- Agregar recurso a un curso");
-        System.out.println("5- Buscar recurso en un curso");
-        System.out.println("6- Eliminar recurso de un curso");
-        System.out.println("7- salir");
+        System.out.println("2- Agregar recurso a un curso");
+        System.out.println("3- Mostar datos de cursos");
+        System.out.println("4- salir");
         System.out.println("=====================================\n");
         System.out.print("Ingrese su opción: ");
     }
 
+    /********************************************************************************** */
     //Funciones de agregar
     public void agregarCurso() throws IOException
     {
@@ -72,11 +86,81 @@ class Colegio
         nombreCurso = lector.readLine();
 
         curso = new Curso (id, capCurso, profJefe, nombreCurso);
-        mapaCursos.put(curso.getCurso(), curso);
+
+        if (!mapaCursos.containsKey(nombreCurso))
+        {
+            mapaCursos.put(curso.getCurso(), curso);
+            System.out.println("Se ha ingresado el curso con exito");
+        }
+        else
+            System.out.println("ALERTA: Ya existe un curso con el mismo nombre");
     }
+
+
+    public void agregarRecursoACurso() throws IOException
+    {   
+        String nombreCurso, nombreRecurso, asignatura;
+        RecursoDigital recursoEducativo;
+        Curso curso;
+
+        System.out.println("Ingrese el nombre del curso: ");
+        nombreCurso = lector.readLine();
+
+        curso = mapaCursos.get(nombreCurso);
+
+        if (curso != null)
+        {
+            System.out.println("Ingrese el nombre del recurso digital: ");
+            nombreRecurso = lector.readLine();
     
+            System.out.println("Ingrese la asignatura donde se usara el recurso digital: ");
+            asignatura = lector.readLine();
     
-    //GETTER y SETEERS
+            recursoEducativo = new RecursoDigital(nombreRecurso, asignatura);
+
+            curso.agregarRecurso(recursoEducativo);
+
+            System.out.println("Se ha ingresado el recurso digital con exito");
+        }
+        else
+        {
+            System.out.println("No se ha podido encontrar el curso");
+        }
+    }
+
+    /********************************************************************************** */
+    //FUNCIONES DE MOSTRAR
+    public void mostrarDatosCursos()
+    {
+        ArrayList <RecursoDigital> recursosDigitales;
+        RecursoDigital recursoActual;
+
+        System.out.println("Cursos en el colegio " + nombreColegio + ":");
+
+        for (Map.Entry<String, Curso> entry : mapaCursos.entrySet())
+        {
+            Curso curso = entry.getValue();
+            System.out.println("ID del Curso: " + curso.getId());
+            System.out.println("Nombre del Curso: " + curso.getCurso());
+            System.out.println("Profesor Jefe: " + curso.getProfJefe());
+            System.out.println("Cantidad de Estudiantes: " + curso.getEstudiantes().length);
+            System.out.println("Recursos Digitales:");
+
+            recursosDigitales = curso.getRecursosDigitales();
+
+            for (int i = 0; i < recursosDigitales.size(); i++)
+            {
+                recursoActual = recursosDigitales.get(i);
+                System.out.println("ID del Recurso: " + recursoActual.getId());
+                System.out.println("Nombre del Recurso: " + recursoActual.getRecurso());
+                System.out.println("Asignatura: " + recursoActual.getAsignatura());
+            }
+            System.out.println("-------------------------------------");
+        }
+    }
+
+    /********************************************************************************** */
+    //GETTERS y SETEERS
     public Curso getCurso(String nombreCurso)
     {
         return mapaCursos.get(nombreCurso);
