8428921a1cf8eab40ef00ac1880afa0fa19ef280
migue <migue@LAPTOP-SJP5JB8Q>
2024-03-29 17:15:50 -0300

    Creacion de funcion para inicializar el sistema

--- proyectoSIA/src/main/java/Colegio.java
+++ proyectoSIA/src/main/java/Colegio.java
@@ -18,12 +18,15 @@ class Colegio
     public void ejecutar() throws IOException
     {
         int opcion = 0;
-
+        
+        inicializarSistema();
         while (opcion != 4)
         {
             mostrarMenu();
             opcion = Integer.parseInt(lector.readLine());
-
+            
+            System.out.println("=====================================\n");
+            
             switch(opcion)
             {
                 case 1:
@@ -49,6 +52,7 @@ class Colegio
                 default:
                     System.out.println("Opción no válida, por favor seleccione otra opción.");
             }
+            System.out.println("\n=====================================\n");
         }
     }
    
@@ -62,9 +66,50 @@ class Colegio
         System.out.println("3- Mostar datos de cursos");
         System.out.println("4- salir");
         System.out.println("=====================================\n");
-        System.out.print("Ingrese su opción: ");
+        System.out.println("Ingrese su opción: ");
     }
 
+    /********************************************************************************** */
+    //FUNCIONES PARA INICIALIZAR
+    public void inicializarSistema()
+    {
+        Curso curso;
+        RecursoDigital recursoEducativo;
+        
+        curso =  new Curso(1, 30, "Arthur Doyle", "1ro basico A");
+        mapaCursos.put(curso.getCurso(), curso);
+        
+        recursoEducativo = new RecursoDigital(10, "Kahoot", "Historia");
+        curso.agregarRecurso(recursoEducativo);
+        
+        recursoEducativo = new RecursoDigital(13, "YouTube", "Ciencias Naturales");
+        curso.agregarRecurso(recursoEducativo);
+        
+        
+        curso =  new Curso(2, 30, "Paulo Coelho", "3ro Medio B");
+        mapaCursos.put(curso.getCurso(), curso);
+        
+        recursoEducativo = new RecursoDigital(53, "YouTube", "Filosofia");
+        curso.agregarRecurso(recursoEducativo);
+        
+        recursoEducativo = new RecursoDigital(85, "PPTs", "Filosofia");
+        curso.agregarRecurso(recursoEducativo);
+        
+        
+        curso =  new Curso(3, 30, "Paula Hawkins", "6to basico A");
+        mapaCursos.put(curso.getCurso(), curso);
+        
+        recursoEducativo = new RecursoDigital(43, "Duolingo", "Ingles");
+        curso.agregarRecurso(recursoEducativo);
+        
+        
+        curso =  new Curso(4, 30, "Abraham Stoker", "8vo basico A");
+        mapaCursos.put(curso.getCurso(), curso);
+        
+        recursoEducativo = new RecursoDigital(66, "Canva", "Artes Visuales");
+        curso.agregarRecurso(recursoEducativo);
+    }
+    
     /********************************************************************************** */
     //Funciones de agregar
     public void agregarCurso() throws IOException
@@ -73,16 +118,16 @@ class Colegio
         String profJefe, nombreCurso;
         Curso curso;
 
-        System.out.print("Ingrese el ID del curso: ");
+        System.out.println("Ingrese el ID del curso: ");
         id = Integer.parseInt(lector.readLine());
 
-        System.out.print("Ingrese la capacidad del curso: ");
+        System.out.println("Ingrese la capacidad del curso: ");
         capCurso = Integer.parseInt(lector.readLine());
 
-        System.out.print("Ingrese el nombre del profesor jefe: ");
+        System.out.println("Ingrese el nombre del profesor jefe: ");
         profJefe = lector.readLine();
 
-        System.out.print("Ingrese el nombre del curso: ");
+        System.out.println("Ingrese el nombre del curso: ");
         nombreCurso = lector.readLine();
 
         curso = new Curso (id, capCurso, profJefe, nombreCurso);
@@ -90,15 +135,16 @@ class Colegio
         if (!mapaCursos.containsKey(nombreCurso))
         {
             mapaCursos.put(curso.getCurso(), curso);
-            System.out.println("Se ha ingresado el curso con exito");
+            System.out.println("\nSe ha ingresado el curso con exito");
         }
         else
-            System.out.println("ALERTA: Ya existe un curso con el mismo nombre");
+            System.out.println("\nALERTA: Ya existe un curso con el mismo nombre");
     }
 
 
     public void agregarRecursoACurso() throws IOException
     {   
+        int idRecurso;
         String nombreCurso, nombreRecurso, asignatura;
         RecursoDigital recursoEducativo;
         Curso curso;
@@ -110,17 +156,18 @@ class Colegio
 
         if (curso != null)
         {
+            System.out.println("Ingrese el ID del recurso digital: ");
+            idRecurso = Integer.parseInt(lector.readLine());
+            
             System.out.println("Ingrese el nombre del recurso digital: ");
             nombreRecurso = lector.readLine();
     
             System.out.println("Ingrese la asignatura donde se usara el recurso digital: ");
             asignatura = lector.readLine();
     
-            recursoEducativo = new RecursoDigital(nombreRecurso, asignatura);
+            recursoEducativo = new RecursoDigital(idRecurso, nombreRecurso, asignatura);
 
             curso.agregarRecurso(recursoEducativo);
-
-            System.out.println("Se ha ingresado el recurso digital con exito");
         }
         else
         {
@@ -135,7 +182,7 @@ class Colegio
         ArrayList <RecursoDigital> recursosDigitales;
         RecursoDigital recursoActual;
 
-        System.out.println("Cursos en el colegio " + nombreColegio + ":");
+        System.out.println("Cursos en el colegio " + nombreColegio + ":\n");
 
         for (Map.Entry<String, Curso> entry : mapaCursos.entrySet())
         {
@@ -144,7 +191,7 @@ class Colegio
             System.out.println("Nombre del Curso: " + curso.getCurso());
             System.out.println("Profesor Jefe: " + curso.getProfJefe());
             System.out.println("Cantidad de Estudiantes: " + curso.getEstudiantes().length);
-            System.out.println("Recursos Digitales:");
+            System.out.println("\nRecursos Digitales:\n");
 
             recursosDigitales = curso.getRecursosDigitales();
 
@@ -153,7 +200,7 @@ class Colegio
                 recursoActual = recursosDigitales.get(i);
                 System.out.println("ID del Recurso: " + recursoActual.getId());
                 System.out.println("Nombre del Recurso: " + recursoActual.getRecurso());
-                System.out.println("Asignatura: " + recursoActual.getAsignatura());
+                System.out.println("Asignatura: " + recursoActual.getAsignatura() + "\n");
             }
             System.out.println("-------------------------------------");
         }
@@ -186,4 +233,3 @@ class Colegio
         this.mapaCursos = mapaCursos;
     }
 }
-
